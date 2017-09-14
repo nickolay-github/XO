8e3dd4ca74c7123e6df9a42f2c65e723b350d146
Nicolay <columbine.bullet@gmail.com>
2017-09-15 00:51:59 +0300

    player created

---
+++ src/nico/xo/model/Player.java
@@ -0,0 +1,22 @@
+package nico.xo.model;
+
+
+public class Player {
+
+    private final String name;
+
+    private final Figure figure;
+
+    public Player(String name, Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+}
